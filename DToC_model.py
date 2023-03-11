e89b614089b8c6cc91ed4c0f66e3d2c98f30b813
ujjwal170888@gmail.com <ujjwal170888@gmail.com>
2023-03-11 15:19:08 +0000

    dtoc model

--- DToC_model.py
+++ DToC_model.py
@@ -22,7 +22,7 @@ display(cust_feature)
 customer_dt_model_id = 'dtmi:dtoc:Customer;1' 
 card_dt_model_id = 'dtmi:dtoc:Card;1' 
 products_dt_model_id = 'dtmi:dtoc:Product;1'
-country_model_id = 'dtmi:dtoc:Country;1'
+country_dt_model_id = 'dtmi:dtoc:Country;1'
 
 context = "dtmi:dtdl:context;2"
 
@@ -50,7 +50,7 @@ class customer_digital_twin_model:
                             'company_stage':'string','legal_structure':'string'}
             telemetry_list = {'age_months':'float','total_txn_count':'float','total_transaction_value':'float','avg_mnthly_txn_value':'float','avg_mnthly_txn_count':'integer','total_card_count':'integer','active_card_count':'integer','inactive_card_count':'integer','suspended_card_count':'integer','visiting_site_count':'integer','Fee_txn_cnt':'integer','Fuel_txn_cnt':'integer','Service_txn_cnt':'integer','churn':'string','importance_score':'float'}
 
-            relationship_list= {"owns":card_dt_model_id,"belongs_to":country_model_id}
+            relationship_list= {"owns":card_dt_model_id,"belongs_to":country_dt_model_id}
 
             content_list = content_list + self.get_properties(property_list) + self.get_telemetry(telemetry_list) + self.get_relationship(relationship_list)
             cust_model_base_dict["contents"] = content_list
@@ -254,6 +254,60 @@ file_name ='product_model.json'
 with open(path+file_name, "w") as fp:
     json.dump(product_model , fp)
 json.dumps(product_model)
+
+# COMMAND ----------
+
+class country_digital_twin_model:
+    '''
+    '''
+    def __init__(self):
+        pass
+    
+    def create_country_model(self):
+        '''
+        '''
+        try:
+            card_model_base_dict = {
+                              "@id":country_dt_model_id,
+                              "@type": "Interface",
+                              "displayName": "Coountry",
+                              "@context": context,
+                              }
+            content_list = []
+            content_dict = {}
+            property_list = {'name':'string','region':'string'}
+            
+            content_list = content_list + self.get_properties(property_list)  
+            card_model_base_dict["contents"] = content_list
+            
+            #json_object = json.dumps(cust_model_base_dict, indent = 4)
+            return card_model_base_dict
+        except Exception as e:
+            print(e)
+
+    def get_properties(self, property_dict):
+        '''
+        '''
+        try:
+            tmp_lst = []
+            for prop_key in property_dict:
+                tmp_dict = {}
+                tmp_dict["@type"] = "Property"
+                tmp_dict["name"] = prop_key
+                tmp_dict["schema"] = property_dict[prop_key]
+                tmp_lst.append(tmp_dict)
+            return tmp_lst
+        except Exception as e:
+            print(e)
+
+# COMMAND ----------
+
+country_model = country_digital_twin_model().create_country_model()
+path ='/dbfs/FileStore/tables/DToC/Model/'
+file_name ='product_model.json'
+with open(path+file_name, "w") as fp:
+    json.dump(country_model , fp)
+json.dumps(country_model)
 
 # COMMAND ----------
 
